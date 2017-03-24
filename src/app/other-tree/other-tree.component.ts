c5bf570b73cf937b60206f1cb4b5797573497599
nebeinstein <nebeinstein@wpi.edu>
2017-03-24 15:56:50 -0400

    man so much stuff was done created the tree component, got that from the codeplay website created the other-tree component, got that from clarkeology.com still confused on how best to do this

---
+++ src/app/other-tree/other-tree.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ft-other-tree',
+  templateUrl: './other-tree.component.html',
+  styleUrls: ['./other-tree.component.css']
+})
+export class OtherTreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
